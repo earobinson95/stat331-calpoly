882e4399adaa9b8abd9321a544d58950cfdc6600
Emily A. Robinson <erobin17@calpoly.edu>
2023-01-24 15:23:32 -0800

    fix url to github repo for sql murder

--- practice-activities/data/sql-murder-setup.R
+++ practice-activities/data/sql-murder-setup.R
@@ -2,7 +2,7 @@ table_names <- c("crime_scene_report", "drivers_license", "facebook_event_checki
                  "get_fit_now_check_in", "get_fit_now_member", "income", "interview",
                  "person")
 
-url_base <- "https://raw.githubusercontent.com/earobinson95/stat-331-calpoly/master/practice-activities/data/sql-murder/"
+url_base <- "https://raw.githubusercontent.com/earobinson95/stat331-calpoly/master/practice-activities/data/sql-murder/"
 
 # For each table name, read the tables in and store them as the name of the table
 purrr::walk(table_names, function(x) {
