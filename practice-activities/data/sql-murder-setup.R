1017f0348f49b9580d1d729dc2a5fc7b566fe1ca
Emily A. Robinson <erobin17@calpoly.edu>
2023-01-24 15:21:37 -0800

    try this for murder-sql-city

---
+++ practice-activities/data/sql-murder-setup.R
@@ -0,0 +1,10 @@
+table_names <- c("crime_scene_report", "drivers_license", "facebook_event_checkin",
+                 "get_fit_now_check_in", "get_fit_now_member", "income", "interview",
+                 "person")
+
+url_base <- "https://raw.githubusercontent.com/earobinson95/stat-331-calpoly/master/practice-activities/data/sql-murder/"
+
+# For each table name, read the tables in and store them as the name of the table
+purrr::walk(table_names, function(x) {
+  assign(x, readr::read_csv(paste0(url_base, x, ".csv")), envir = .GlobalEnv)
+})
