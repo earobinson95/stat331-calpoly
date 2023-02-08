6f6bb9337110449ba6d206d891db0d8c44819e72
Emily A. Robinson <erobin17@calpoly.edu>
2023-02-07 18:10:07 -0800

    update midterm exam expectations

---
+++ lecture-slides/05-special-data-types/regex-you-try-test.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+
+test_vec <- c("zebra", "xray", "apple", "yellow", "color", "colour", "summarize", "summarise")
+
+# end with a vowel
+str_subset(test_vec, pattern = "[aeiou]$")
+
+# start with x, y, or z
+str_view_all(test_vec, pattern = "^[xyz]")
+str_subset(test_vec, pattern = "^[xyz]")
+
+# do not contain x, y, or z
+str_view_all(test_vec, pattern = "[^xyz]")
+
+str_extract(test_vec, pattern = "[^xyz]")
+str_extract_all(test_vec, pattern = "[^xyz]")
+
+str_replace(test_vec, pattern = "[^xyz]", replace = "X")
+str_replace_all(test_vec, pattern = "[^xyz]", replace = "X")
+
+# contain British spelling (e.g. color vs colour)
+# British English words ending in ‘our’ usually end in ‘or’ in American English
+# Verbs in British English that can be spelled with either ‘ize’ or ‘ise’ at the end 
+# are always spelled with ‘ize’ at the end in American English:
+str_view_all(test_vec, pattern = "([a-zA-Z]+ise$|[a-zA-Z]+our$)")
